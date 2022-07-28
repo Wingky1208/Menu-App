bc10ab9c8a9017150c3d9384b16e88a0d9664da3
Abdalla Jama <abdallajama201@gmail.com>
2022-07-28 14:28:47 -0400

    frontend layout

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+
+const Profile = () => {
+  return (
+    <div className="container">
+      <Nav />
+      <Footer />
+    </div>
+  );
+};
+
+export default Profile;
