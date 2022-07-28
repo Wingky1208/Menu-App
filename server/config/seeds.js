3a9b0ced2f7455f876d5d8ff6b4e93ac4e5cf555
James <jameslfidlin@gmail.com>
2022-07-28 13:11:18 -0400

    still working on seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -45,7 +45,7 @@ db.once('open', async () => {
       category: categories[0]._id
     },
     {
-      name: 'Cheddar Biscuits',
+      name: 'Pigs in a blanket',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'cookie-tin.jpg',
@@ -53,12 +53,380 @@ db.once('open', async () => {
       category: categories[0]._id
     },
     {
-      name: 'Cheddar Biscuits',
+      name: 'Spinach cheese dip with chips',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'cookie-tin.jpg',
       price: 2.99,
       category: categories[0]._id
+    },
+    {
+      name: 'Onion rings',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'French fries',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Baked potato',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Breadsticks',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Salad',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Quick Kimchi (Mak Gimchi)',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Fresh rolls',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Spring rolls',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Deep fried cheese dumplings',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[0]._id
+    },
+    {
+      name: 'Pizza',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Chicken pot pie',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Mashed potatoes',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Fried chicken',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Burgers',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Lobster rolls',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Cajun Shrimp',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Grilled Fish',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Chicken soup',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Meatloaf',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Lasagna',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Spaghetti with meatballs',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Chicken burger',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Chicken parmesan',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Chicken Pesto',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Burger Sliders',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[1]._id
+    },
+    {
+      name: 'Caramelized Onions',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Thousand Island Dressing',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Bacon Jam',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Guacamole',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Sauteed Mushrooms',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Tomato Jam',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Dijon Mustard',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Bourbon Barbecue Sauce',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Onion Jam',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Blue Cheese Sauce',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Homemade Ketchup',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[2]._id
+    },
+    {
+      name: 'Pepperoni',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: 'Mushroom',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: 'Extra cheese',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: 'Sausage',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: 'Onion',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: 'Black olives',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: 'Green pepper',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: 'Fresh garlic',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: '',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
+    },
+    {
+      name: '',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      price: 2.99,
+      category: categories[3]._id
     },
   ]);
 
