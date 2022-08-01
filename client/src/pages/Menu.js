bcdb19d9703d2dd9455c5d08fafc1de4ccaf51f7
James <jameslfidlin@gmail.com>
2022-08-01 14:07:15 -0400

    style update 5, top dishes cards

---
+++ client/src/pages/Menu.js
@@ -0,0 +1,11 @@
+import HTMLFlipBook from "react-pageflip";
+
+
+  return (
+    <HTMLFlipBook width={300} height={500}>
+      <div className="demoPage">Page 1</div>
+      <div className="demoPage">Page 2</div>
+      <div className="demoPage">Page 3</div>
+      <div className="demoPage">Page 4</div>
+    </HTMLFlipBook>
+  );
